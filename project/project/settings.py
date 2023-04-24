6ac4bf8dd1da5e99db666a12527fd11cd5b39469
malfoyz <the13mufasa@gmail.com>
2023-04-24 17:56:22 +0700

    Add .env

--- project/project/settings.py
+++ project/project/settings.py
@@ -10,7 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+# Loading ENV
+env_path = Path('.') / '.env'
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qhyak3y9mut$l=kmj0mx^@cdor^b#+a&#v4$gt&!qd(nxm7421'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
