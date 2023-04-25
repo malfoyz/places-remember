ec08f9bbbe013c72c3ac55dfacc085b4004c51c2
malfoyz <the13mufasa@gmail.com>
2023-04-25 10:12:52 +0700

    Add basic templates

--- project/project/settings.py
+++ project/project/settings.py
@@ -45,6 +45,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
+
+    # my apps
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
